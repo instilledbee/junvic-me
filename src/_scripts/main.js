76a77680f9f6d0644d0cdddb25377447b1188851
Junvic <junvic.valdez@instilledbee.net>
2017-10-08 20:25:09 +0800

    basic sticky navbar

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -4,9 +4,38 @@
 'use strict';
 
 var $ = require('jquery');
-var Link = require('../_modules/link/link');
 
 $(function() {
-    new Link(); // Activate Link modules logic
-    console.log('Welcome to Yeogurt!');
+    var navs = ['#about', '#projects', '#contact'];
+
+    // hand-rolled scrollspy handlers
+    $(window).scroll(function() {
+        if(!isElementInView($('header'))) {
+            $('nav').addClass('scrolled');
+        } else {
+            $('nav').removeClass('scrolled');
+        }
+    });
+
+    // smooth scroll effect
+    // https://stackoverflow.com/a/6677069
+    $("nav a").click(function() {
+        var linkTarget = $(this).attr('href');
+
+        $('html, body').animate({scrollTop: $(linkTarget).offset().top}, 500);
+    });
 });
+
+// source: https://stackoverflow.com/a/488073
+function isElementInView(element, fullyInView) {
+    var pageTop = $(window).scrollTop();
+    var pageBottom = pageTop + $(window).height();
+    var elementTop = $(element).offset().top;
+    var elementBottom = elementTop + $(element).height();
+
+    if (fullyInView === true) {
+        return ((pageTop < elementTop) && (pageBottom > elementBottom));
+    } else {
+        return ((elementTop <= pageBottom) && (elementBottom >= pageTop));
+    }
+};
